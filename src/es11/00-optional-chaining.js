84aad2cc6dcf9bbbb859dce9256d8c572650009f
= <=>
2022-11-22 19:42:11 -0400

    Terminamos con la version es11

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,10 @@
+const users = {
+    milortjem: {
+        country: "VZ"
+    },
+    katherin: {
+        country: "VZ"
+    }
+}
+
+console.log(users?.bebeloper?.country);
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 128946098124908126471908462908n;
+const anotherBigNumber = BigInt(128946098124908126471908462908);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
